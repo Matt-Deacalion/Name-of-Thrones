b578635073a33fbf3080dd0fd79a47c978247c25
Matt Deacalion Stevens <matt@dirtymonkey.co.uk>
2014-06-23 21:40:22 +0100

    Improve project pitch.

--- game_of_thrones/__init__.py
+++ game_of_thrones/__init__.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
 """
-Command line tool to generate words that
-sound like characters from Game of Thrones.
+Command line tool to generate words that sound like
+characters from Game of Thrones. Good for project names.
 """
 __author__ = 'Matt Deacalion Stevens'
 __version__ = '0.1.1'
