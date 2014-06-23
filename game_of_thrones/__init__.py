7af8215ba31a0a9104a5f87d6f1bea8e9d713f6a
Matt Deacalion Stevens <matt@dirtymonkey.co.uk>
2014-06-23 21:36:58 +0100

    Bump version to `0.1.1`

--- game_of_thrones/__init__.py
+++ game_of_thrones/__init__.py
@@ -5,7 +5,7 @@ Command line tool to generate words that
 sound like characters from Game of Thrones.
 """
 __author__ = 'Matt Deacalion Stevens'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 import random
 import string
